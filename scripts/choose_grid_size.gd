1338d230197f92ae73422400578528264d623d71
PooOrmond <lovememon14@gmail.com>
2025-11-02 19:35:03 +0800

    init

--- scripts/choose_grid_size.gd
+++ scripts/choose_grid_size.gd
@@ -2,8 +2,7 @@ extends Control
 
 @onready var click: AudioStreamPlayer2D = $click
 
-func _on_texture_button_pressed():
+func _on_backbutton_pressed() -> void:
 	click.play()
 	await get_tree().create_timer(0.1).timeout
-	get_tree().change_scene_to_file("res://scenes/choose_grid_size.tscn")
-	
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
