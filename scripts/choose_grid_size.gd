f3df9a5dc38a3e3bf100d794690e0fb909ce3e24
PooOrmond <lovememon14@gmail.com>
2025-11-03 13:27:25 +0800

    init

--- scripts/choose_grid_size.gd
+++ scripts/choose_grid_size.gd
@@ -6,3 +6,9 @@ func _on_backbutton_pressed() -> void:
 	click.play()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_x_7_button_pressed() -> void:
+	click.play()
+
+func _on_x_10_button_pressed() -> void:
+	click.play()
